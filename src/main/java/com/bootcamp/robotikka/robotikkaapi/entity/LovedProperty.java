23506f00ba9264f6d6d69e3c2010b6c72bc1d70f
ozen-hub <info.ozenhub@gmail.com>
2022-12-28 20:45:06 +0530

    variable declaration completed

---
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/entity/LovedProperty.java
@@ -0,0 +1,16 @@
+package com.bootcamp.robotikka.robotikkaapi.entity;
+
+import java.util.Date;
+
+public class LovedProperty {
+    private String propertyId;
+    private Date savedDate;
+  /*  user_property_id;
+    product_property_id*/
+}
+
+
+/*property_id;
+ saved_date;
+  user_property_id;
+   product_property_id*/
