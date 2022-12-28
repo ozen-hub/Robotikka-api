98541af000d2cb192513c1dc87d56cb7b0e23525
ozen-hub <info.ozenhub@gmail.com>
2022-12-28 19:50:48 +0530

    Initial commit

---
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/RobotikkaApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.robotikka.robotikkaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotikkaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotikkaApiApplication.class, args);
+	}
+
+}
