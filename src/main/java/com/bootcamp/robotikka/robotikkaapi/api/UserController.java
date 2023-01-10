1b151cd1320a1b1093aea54934280f516be21ec1
ozen-hub <info.ozenhub@gmail.com>
2023-01-10 21:20:20 +0530

    2

--- src/main/java/com/bootcamp/robotikka/robotikkaapi/api/UserController.java
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/api/UserController.java
@@ -1,11 +1,14 @@
 package com.bootcamp.robotikka.robotikkaapi.api;
 
+import com.bootcamp.robotikka.robotikkaapi.dto.request.RequestUserDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+    @PostMapping("/visitor/register")//
+    public void createUser(@RequestBody RequestUserDTO dto){
+        // save
+    }
 }
