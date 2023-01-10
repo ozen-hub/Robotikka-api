c7659d3b6063a234d4bc1d94c2c4119532f6adb2
ozen-hub <info.ozenhub@gmail.com>
2023-01-10 21:36:19 +0530

    4

---
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/util/Generator.java
@@ -0,0 +1,16 @@
+package com.bootcamp.robotikka.robotikkaapi.util;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Generator {
+    private static String ALPH ="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public String generatePrefix(){
+        //
+    }
+    public int generateRand(){
+        //==>
+    }
+
+}
