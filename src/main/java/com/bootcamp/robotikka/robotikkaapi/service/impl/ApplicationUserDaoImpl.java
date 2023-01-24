46461bb8ef1cbf6145e6e0e34438de1347a27a3d
ozen-hub <info.ozenhub@gmail.com>
2023-01-24 10:06:55 +0530

    19

---
+++ src/main/java/com/bootcamp/robotikka/robotikkaapi/service/impl/ApplicationUserDaoImpl.java
@@ -0,0 +1,63 @@
+package com.bootcamp.robotikka.robotikkaapi.service.impl;
+
+import com.developersatck.security.dao.ApplicationUserDao;
+import com.developersatck.security.dto.ApplicationUser;
+import com.google.common.collect.Lists;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.developersatck.security.config.permission.ApplicationUserRole.*;
+
+@Repository
+public class ApplicationUserDaoImpl implements ApplicationUserDao {
+    /*actual database*/
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationUserDaoImpl(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> findApplicationUserByUsername(String username) {
+        return geApplicationUsers().stream().filter(e->e.getUsername().equals(username))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> geApplicationUsers(){
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+               new ApplicationUser(
+                       USER.getGrantedAuthorities(),
+                       "tharaka",
+                       passwordEncoder.encode("1234"),
+                       true,
+                       true,
+                       true,
+                       true
+               ),
+               new ApplicationUser(
+                       MANAGER.getGrantedAuthorities(),
+                       "ayesh",
+                       passwordEncoder.encode("1234"),
+                       true,
+                       true,
+                       true,
+                       true
+               ),
+               new ApplicationUser(
+                       ADMIN.getGrantedAuthorities(),
+                       "nalaka",
+                       passwordEncoder.encode("1234"),
+                       true,
+                       true,
+                       true,
+                       true
+               )
+        );
+        return applicationUsers;
+    }
+
+}
